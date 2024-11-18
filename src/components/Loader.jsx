9b810ca748113fd887744c3e3b80dacd9d6de871
Barry210-hub <umarzafar49678@gmail.com>
2024-11-18 22:48:24 +0500

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Html } from '@react-three/drei'
+import { useProgress } from '@react-three/drei'
+import { progress } from 'framer-motion'
+
+
+
+const CanvasLoader = () => {
+
+const {progress} = useProgress();
+
+  return (
+    <Html>
+      <span className='canvas-load'  ></span>
+<p style={
+  {
+    fontSize:14,
+    color: '#f1f1f1',
+    fontWeight : 800,
+    marginTop : 40
+  }
+} >  {progress.toFixed(2)}%
+
+</p>
+    </Html>
+  )
+}
+
+export default CanvasLoader;
---
+++ src/constants/index.js
@@ -0,0 +1,265 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Frontend Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    // {
+    //   name: "TypeScript",
+    //   icon: typescript,
+    // },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    // {
+    //   name: "Redux Toolkit",
+    //   icon: redux,
+    // },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    // {
+    //   name: "figma",
+    //   icon: figma,
+    // },
+    // {
+    //   name: "docker",
+    //   icon: docker,
+    // },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      company_name: "E-Commerence",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Frontend Developer",
+      company_name: "Food del",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "Passionate about creating visually stunning and highly functional front-end designs, ensuring a seamless user experience across devices",
+        "Proficient in HTML, CSS, JavaScript, and modern frameworks like React and Vue.js, crafting interactive and responsive web interfaces",
+        "Skilled in translating design mockups into clean, pixel-perfect code, delivering engaging and intuitive layouts",
+        "Focused on enhancing user engagement through smooth animations, optimized performance, and modern design trends."
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "Shopify",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        "I specialize in creating vibrant and user-friendly Shopify stores, bringing unique designs to life with ease.",
+        "Expert in customizing Shopify themes, integrating apps, and optimizing the shopping experience for better user engagement.",
+        "Skilled at building fully functional e-commerce websites with smooth checkout processes, payment gateway integrations, and inventory management.",
+      
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Shooping Website",
+      description:
+        "This is an online shopping platform where customers can purchase clothing for men, women, and kids. The website offers a wide variety of trendy and high-quality clothes, making shopping convenient and accessible.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://barry210-hub.github.io/vite-react-deploy/",
+    },
+    {
+      name: "Food del",
+      description:
+        "This is an online food delivery platform where customers can order a variety of delicious meals. The website offers a wide range of cuisines, from local favorites to international dishes, all available for delivery right to your doorstep.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };

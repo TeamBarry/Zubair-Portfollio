e7820f07f5afdcae8ccc4c0a240281012f98dac0
Zubair Zafar <umarzafar49678@gmail.com>
2025-06-20 02:48:26 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import {About , Contact , Experience , Feedbacks , Hero ,Navbar ,Tech , Works , 
   return (
     <BrowserRouter>
    <div  className="relative bg-primary" >
-     <div 
+     <div className="bg-hero-pattern bg-cover bg-no-repeat"
           
     
     >
-{/*       className="bg-hero-pattern bg-cover bg-no-repeat" */}
+{/*        */}
       <Navbar/>
       <Hero/>
 
